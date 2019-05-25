5c5cf0cc516285c56edd2700f197cdf275ef2649
Stephan <stephan.startsev@gmail.com>
2019-05-25 19:59:04 +0300

    doesn't work

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+
+      <section>
+        <div class="container">
+
+          <!-- modal with validate -->
+          <button class="btn btnPrimary" @click="modalValidate = !modalValidate">Show modal with form + validate</button>
+          <modalValidate v-show="modalValidate" @close="modalValidate = false"/>
+
+          <modalCreate v-show="modalCreate" @openCreateWindow="openCreateWindow"/>
+
+        </div>
+      </section>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+import modals from '@/components/UI/Modal.vue'
+import modalValidate from '@/components/ModalValidate.vue'
+import modalCreate from '@/components/ModalCreate.vue'
+
+export default {
+  components: { modals, modalValidate, modalCreate },
+  data () {
+    return {
+      modalValidate: false,
+      modalCreate: false
+    }
+  },
+  methods: {
+    openCreateWindow () {
+      this.$emit('openCreateWindow', this.modalCreate)
+    }
+  }
+}
+</script>
